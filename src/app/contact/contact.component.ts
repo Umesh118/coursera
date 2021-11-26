e398dba335f82dcef9da15aef2e799113ad36343
Umesh Dhangare <umesh.dhangare@nobrainsolutions.com>
2021-11-26 11:48:47 +0530

    Reactive Forms Part 3

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -2,6 +2,17 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Feedback, ContactType } from '../shared/feedback';
 
+interface Map {
+  [key: string]: string
+}
+
+interface Map2 {
+  [key: string]: {
+    [key: string]: string
+  }
+}
+
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -16,18 +27,52 @@ export class ContactComponent implements OnInit {
     this.createForm();
   }
 
-  ngOnInit(): void {}
+
+
+  formErrors: Map = {
+    'firstname': '',
+    'lastname': '',
+    'telnum': '',
+    'email': ''
+  };
+
+  validationMessages: Map2 = {
+    'firstname': {
+      'required': 'First Name is required',
+      'minlength': 'First Name must be atleasr 2 charactors long.',
+      'maxlength': 'First Name cannot be 25 charactors long.'
+    },
+    'lastname': {
+      'required': 'Last Name is required',
+      'minlength': 'Last Name must be atleasr 2 charactors long.',
+      'maxlength': 'Last Name cannot be 25 charactors long.'
+    },
+    'telnum': {
+      'required': 'Tel. number is required.',
+      'pattern': 'Tel. number must contain numbers.'
+    },
+    'email': {
+      'required': 'Email is required.',
+      'email': 'Email not in valid format.'
+    }
+  };
+
+  ngOnInit(): void { }
 
   createForm() {
     this.feedbackForm = this.fb.group({
-      firstname: ['', Validators.required],
-      lastname: ['', Validators.required],
-      telnum: ['', Validators.required],
-      email: ['', Validators.required],
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      telnum: ['', [Validators.required, Validators.pattern]],
+      email: ['', [Validators.required, Validators.email]],
       agree: false,
       contacttype: 'None',
       message: '',
     });
+
+    this.feedbackForm.valueChanges.subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged() // reset validation messages now
   }
 
   onSubmit() {
@@ -43,5 +88,25 @@ export class ContactComponent implements OnInit {
       message: '',
     });
     this.feedbackFormDirective.resetForm();
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.feedbackForm) { return; }
+    const form = this.feedbackForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        // clear previous error messages (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
   }
 }
